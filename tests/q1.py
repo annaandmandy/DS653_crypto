f949612b9c8e0a86230c8aa56797882d40d9c8b1
annaandmandy <annaandtommy1004@gmail.com>
2025-02-10 18:07:42 -0500

    add hw3 test case

--- tests/q1.py
+++ tests/q1.py
@@ -2,16 +2,22 @@ OK_FORMAT = True
 
 test = {   'all_or_nothing': True,
     'name': 'q1',
-    'points': 3,
-    'suites': [   {   'cases': [   {   'code': '>>> from os import urandom\n'
-                                               '>>> from binascii import hexlify\n'
-                                               '>>> from hashlib import sha256\n'
-                                               ">>> assert hexlify(sha256(hw2_q1().to_bytes(1, 'big') + b'5d7a504fb283632efeed2d5c56ef6015').digest()) == "
-                                               "b'49cf57774e3dd060aadad260750b3b88ded31c8199855ed311ff3bfd00ab0ee9', 'incorrect response, try again'\n",
-                                       'failure_message': 'Try again',
+    'points': 6,
+    'suites': [   {   'cases': [   {   'code': '>>> def q1_public_tests():\n'
+                                               '...     tx = Transaction(TransactionPayload(public_keys[0], public_keys[1], 1), '
+                                               "'edd6bb60e8486f9747f911a6d7340c275d5a417c3823952715a18a64d3577325413cf544e8f48e6c30a312649200c57b72d7eaa4721abf376dbd0dc799db6c3a')\n"
+                                               "...     assert isValidTransaction(tx) == True, 'Should accept a valid transaction'\n"
+                                               '...     tx.signature = '
+                                               "'00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'\n"
+                                               "...     assert isValidTransaction(tx) == False, 'Should reject an invalid signature'\n"
+                                               '...     tx = Transaction(TransactionPayload(public_keys[2], public_keys[3], 1), '
+                                               "'33bcaaa920207125ec72ea7f9e5fbddb993479dbe7122c6dfcf0a96f2e646dd90ff73c90c784f473480e32fd2fb68735af86458bc0434e5958f025cc9e0ddd59')\n"
+                                               "...     assert isValidTransaction(tx) == True, 'Should accept a valid transaction'\n"
+                                               "...     tx.signature = '2222222222222222222222222222222222222222222222222222222222222222'\n"
+                                               "...     assert isValidTransaction(tx) == False, 'Should reject a signature that is invalid and of the incorrect length'\n"
+                                               '>>> q1_public_tests()\n',
                                        'hidden': False,
-                                       'locked': False,
-                                       'success_message': 'Satisfactory'}],
+                                       'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
--- tests/q5.py
+++ tests/q5.py
@@ -3,15 +3,32 @@ OK_FORMAT = True
 test = {   'all_or_nothing': True,
     'name': 'q5',
     'points': 3,
-    'suites': [   {   'cases': [   {   'code': '>>> from os import urandom\n'
-                                               '>>> from binascii import hexlify\n'
-                                               '>>> from hashlib import sha256\n'
-                                               ">>> assert hexlify(sha256(hw2_q5().to_bytes(1, 'big') + b'6e12018d726a6d4a19f910eb0054923b').digest()) == "
-                                               "b'f4ee37b1012e0888aeab4585f2ee00a48d550302210bc4f735a5d84c025d0233', 'incorrect response, try again'\n",
-                                       'failure_message': 'Try again',
+    'suites': [   {   'cases': [   {   'code': '>>> def q5_test():\n'
+                                               "...     secret_key = ECC.generate(curve='P-256', randfunc=lambda x: b'0' * x)\n"
+                                               '...     public_keys = '
+                                               "['044d4385fe08e0eb94c524bdd3682c9c5ae358f52402df02418d0ba6cf6889289a27bd56a666df7dc595c98da1f22958009ae17c52fd290185d2e2bf11140a0a5e', "
+                                               "'04be4a2555cc8ed29989eeddde3d8e4d41458d02dce047aac2e6af2d58688c5beae7163eb157d1fd3c3d282abab7172b0f0d63274ebe721d31c5d72a83741df170', "
+                                               "'04a3fc575afc4ad2ca2cbeed50b52fedf049d317b790aa33e7f8182aec028453082d4c4ffd4c99c7e6d11c134e81020c57ca0fbf0bf3406bf4457dc72d8c994a65', "
+                                               "'04abc5bd2786ea5144122ccf43b89a557e1d36e2773c2b8986d9819a22c2e6540b3d5da692504ccc29fbf774435312afdda2a360d5160329cb326f6d47db29f50b', "
+                                               "'046a921bed68e07e66f38a5137b3784372cfd4507e6451e1dfe3760f08649750e7891cd30339b730c0280738c60233d3c99eb78454340ff363cafbf1e6f0cfce5e']\n"
+                                               '...     blockchain = [gen_block_0(), gen_block_1()]\n'
+                                               '...     players_balance = [1, 0, 1, 0, 0]\n'
+                                               '...     dishonest_blockchain = generate_dishonest_blockchain()\n'
+                                               "...     assert len(dishonest_blockchain) >= 3, 'Dishonest blockchain should contain at least 3 blocks'\n"
+                                               '...     for block in dishonest_blockchain:\n'
+                                               "...         assert verifyBlock(block) == True, 'Blocks in the dishonest blockchain should be valid'\n"
+                                               '...     is_A_to_E_two_coins = False\n'
+                                               '...     for (index, block) in enumerate(dishonest_blockchain):\n'
+                                               '...         for trans in block.transactions:\n'
+                                               '...             payload = trans.transaction_payload\n'
+                                               '...             assert not (index == 0 and payload.sender_public_key == public_keys[0] and (payload.receiver_public_key == public_keys[1]) and '
+                                               "(payload.amount == 1)), 'Must remove the A->B transaction from the first block'\n"
+                                               '...             if payload.sender_public_key == public_keys[0] and payload.receiver_public_key == public_keys[4] and (payload.amount == 2):\n'
+                                               '...                 is_A_to_E_two_coins = True\n'
+                                               "...     assert is_A_to_E_two_coins == True, 'Must create an A->E transaction that pays 2 coins'\n"
+                                               '>>> q5_test()\n',
                                        'hidden': False,
-                                       'locked': False,
-                                       'success_message': 'Satisfactory'}],
+                                       'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
